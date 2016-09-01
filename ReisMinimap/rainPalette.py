1cb049590e1e4e16093417a53ab6921028216af0
beatorizu <betrizaf@gmail.com>
2016-09-01 10:36:36 -0300

    Create first commit
    Added code to customise map plot

---
+++ ReisMinimap/rainPalette.py
@@ -0,0 +1,9 @@
+Accumulated = ['#fffeff', '#b43214', '#f05a32', '#ff9141', '#ffd750', '#fff0b4',
+               '#e6ffe6', '#aae6b4', '#6ee66e', '#06be0a', '#058c00', '#005000',
+               '#002300']
+
+Anomaly = ['#d20000', '#f03c00', '#fa7828', '#faa03c', '#ffd04e', '#fefefe',
+           '#b4f0fa', '#96d2fa', '#50a5f5', '#1e6ef5', '#0000aa']
+
+GloboAcc = ['#fafafa', '#c4e3ff', '#9ac2fd', '#69aafe', '#598efe', '#486aff',
+            '#0f42ff', '#0000ca']
---
+++ ReisMinimap/reisMinimap.py
@@ -0,0 +1,50 @@
+from matplotlib import use
+use('Agg')
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import interp, Basemap
+from numpy import meshgrid
+from numpy.ma import MaskedArray
+import seaborn as sns
+
+
+class Minimap:
+
+    def __init__(self, lon, lat, mlon, mlat):
+        self.lon = lon
+        self.lat = lat
+        self.mlon = mlon
+        self.mlat = mlat
+        self.basemap = Basemap(urcrnrlat=mlat.max(),
+                               urcrnrlon=mlon.max(),
+                               llcrnrlat=mlat.min(),
+                               llcrnrlon=mlon.min(),
+                               resolution='l')
+        self.x, self.y = self.basemap(lon, lat)
+        self.x2, self.y2 = self.basemap(*meshgrid(mlon, mlat))
+
+    def maskData(self, data, mask=None):
+        """
+        This function apply a mask to a data array
+
+        Parameters
+        ----------
+        data:numpy.ndarray
+        mask:numpy.ndarray or None
+
+        Returns
+        -------
+            None
+
+        """
+        result = interp(data, self.x, self.y, self.x2, self.y2)
+        self.masked_var = MaskedArray(result, mask=mask)
+
+    def renderMap(self, waypoint, shape=[], bounds=[0, 7, 42], cmap=None, norm=None, extend='both', name='NOME',
+                  transparent=False):
+        plt.clf()
+        self.basemap.contourf(self.x2, self.y2, self.masked_var, bounds, cmap=cmap, norm=norm, extend=extend)
+        for s in shape:
+            self.basemap.readshapefile(s, name)
+        self.basemap.colorbar(location='bottom')
+        self.basemap.imshow(self.masked_var)
+        plt.savefig(waypoint, transparent=transparent, dpi=200, bbox_inches='tight', pad_inches=0.1)
